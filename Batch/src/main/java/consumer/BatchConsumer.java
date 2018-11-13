df8a4fb1f68504dda861c9fbdc247b01ad65a846
veeranjaneyulu <papasani.soft@gmail.com>
2018-11-13 21:07:12 +0530

    commit

---
+++ Batch/src/main/java/consumer/BatchConsumer.java
@@ -0,0 +1,48 @@
+package consumer;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+public class BatchConsumer {
+public static void main(String []args)throws MQClientException {
+    DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_3");
+
+    consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+    consumer.subscribe("Topic", "TagA || TagC || TagD");
+
+    consumer.registerMessageListener(new MessageListenerOrderly() {
+        AtomicLong consumeTimes = new AtomicLong(0);
+
+        @Override
+        public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+            context.setAutoCommit(false);
+            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+            this.consumeTimes.incrementAndGet();
+            if ((this.consumeTimes.get() % 2) == 0) {
+                return ConsumeOrderlyStatus.SUCCESS;
+            } else if ((this.consumeTimes.get() % 3) == 0) {
+                return ConsumeOrderlyStatus.ROLLBACK;
+            } else if ((this.consumeTimes.get() % 4) == 0) {
+                return ConsumeOrderlyStatus.COMMIT;
+            } else if ((this.consumeTimes.get() % 5) == 0) {
+                context.setSuspendCurrentQueueTimeMillis(3000);
+                return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
+            }
+
+            return ConsumeOrderlyStatus.SUCCESS;
+        }
+    });
+
+    consumer.start();
+    System.out.printf("Consumer Started.%n");
+}
+}
